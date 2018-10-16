0cb87684bc8df65f9a3bd1d2b9b575f946d3ff6b
YeJin Kim <yejin8861@naver.com>
2018-10-16 01:00:44 -0700

    added preprocessing

--- myapp.c
+++ myapp.c
@@ -2,10 +2,17 @@
 #include"func2.h"
 #include<stdio.h>
 
+
+void preprocessing()
+{
+  printf("preprocessing\n");
+}
+
 int main(void)
 {
   func1();
   func2();
+  preprocessing();
 
   return 0;
 }
